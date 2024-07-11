2662feb6af972e2de58dbd578f24c1cd8329200b
Jacob Rodriguez <jacob_rod1231@outlook.com>
2024-07-11 13:56:36 -0400

    Add button to clear search results

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,13 +1,25 @@
-import React from "react";
 import styles from "./SearchBar.module.css";
 
-function SearchBar() {
+function SearchBar({ value, onChange, getSearchResults, setSearchResults }) {
+  const clearSearchResults = () => {
+    setSearchResults([]);
+  };
+
   return (
-    <div>
-      <div className={styles.SearchBar}>
-        <input placeholder="Enter A Song, Album, or Artist" />
-        <button className={styles.SearchButton}>SEARCH</button>
-      </div>
+    <div className={styles.SearchBar}>
+      <input
+        type="text"
+        placeholder="Enter A Song, Album, or Artist"
+        value={value}
+        onChange={onChange}
+      />
+      <button className={styles.SearchButton} onClick={getSearchResults}>
+        SEARCH
+      </button>
+      <br />
+      <button className={styles.ClearSearchButton} onClick={clearSearchResults}>
+        Clear Search Results
+      </button>
     </div>
   );
 }
