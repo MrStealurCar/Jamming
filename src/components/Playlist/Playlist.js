d7045b56180818c57ff4b77b489109d94ed1431c
Jacob Rodriguez <jacob_rod1231@outlook.com>
2024-07-11 09:05:17 -0400

    Playlist component working

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -1,13 +1,31 @@
 import React from "react";
 import styles from "./Playlist.module.css";
 import Tracklist from "../Tracklist/Tracklist";
-function Playlist() {
+function Playlist({
+  playlistTitle,
+  playlistTracks,
+  setPlaylistTitle,
+  setPlaylistTracks,
+  onRemove,
+  onSave,
+}) {
+  const handlePlaylistNameChange = (e) => {
+    setPlaylistTitle(e.target.value);
+  };
+
   return (
     <div>
       <div className={styles.Playlist}>
-        <input value="New Playlist" />
-        <Tracklist />
-        <button className={styles.PlaylistSave}>SAVE TO SPOTIFY</button>
+        <input value={playlistTitle} onChange={handlePlaylistNameChange} />
+
+        <Tracklist
+          tracks={playlistTracks}
+          isRemoval={true}
+          onRemove={onRemove}
+        />
+        <button onClick={onSave} className={styles.PlaylistSave}>
+          SAVE TO SPOTIFY
+        </button>
       </div>
     </div>
   );
